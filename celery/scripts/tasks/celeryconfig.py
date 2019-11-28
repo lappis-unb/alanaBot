5d25228ab1d4118096815abf27e7784f85559532
João Vitor <joaovytor0@gmail.com>
2019-11-28 10:57:50 -0300

    Add env config file for celery

--- celery/scripts/tasks/celeryconfig.py
+++ celery/scripts/tasks/celeryconfig.py
@@ -14,33 +14,37 @@ timezone = 'America/Sao_Paulo'
 enable_utc = False
 include = ['tasks.celerytasks']
 
-# populate_db 30 08
-# google_forms 45 08
-# gsheet_report 50 08
-# send_notification 00 09
 beat_schedule = {
     'seed_db': {
         'task': 'tasks.celerytasks.seed_db',
         'schedule': crontab(hour=8, minute=30,
-                            day_of_week='wed'),
+                            day_of_week='mon,tue,'
+                                        'wed,thu,'
+                                        'fri'),
         'args': ()
     },
     'seed_google_forms': {
         'task': 'tasks.celerytasks.seed_google_forms',
-        'schedule': crontab(hour=8, minute=45,
-                            day_of_week='wed'),
+        'schedule': crontab(hour=8, minute=40,
+                            day_of_week='mon,tue,'
+                                        'wed,thu,'
+                                        'fri'),
         'args': ()
     },
     'seed_gsheet_report': {
         'task': 'tasks.celerytasks.seed_gsheet_report',
-        'schedule': crontab(hour=8, minute=50,
-                            day_of_week='wed'),
+        'schedule': crontab(hour=8, minute=45,
+                            day_of_week='mon,tue,'
+                                        'wed,thu,'
+                                        'fri'),
         'args': ()
     },
     'send_notification': {
         'task': 'tasks.celerytasks.send_notification',
-        'schedule': crontab(hour=9, minute=00,
-                            day_of_week='wed'),
+        'schedule': crontab(hour=9, minute=0,
+                            day_of_week='mon,tue,'
+                                        'wed,thu,'
+                                        'fri'),
         'args': ()
     },
 }
