2300100b1e243f532ddf6f3a5c23eb3d52a03953
João Vitor Ramos <joaovytor0@gmail.com>
2019-12-13 20:40:42 -0500

    Update deploy config

--- init_production.sh
+++ init_production.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-domains=(alanabot.lappis.rocks kibana.alanabot.lappis.rocks)
+domains=(kibana.alanabot.lappis.rocks)
 rsa_key_size=4096
-data_path="./src/certbot"
+data_path="./src/data/certbot"
 email="" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 composefile=./docker-compose.deploy.yml
@@ -21,8 +21,9 @@ fi
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
   echo "### Downloading recommended TLS parameters ..."
   mkdir -p "$data_path/conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
+  echo "testing file $(ls $data_path/conf/options-ssl-nginx.conf)"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
   echo
 fi
 
@@ -31,6 +32,7 @@ for domain in ${domains[@]}
 do
   echo "### Creating dummy certificate for $domain ..."
   path="/etc/letsencrypt/live/$domain"
+  echo "TESTING PATH ### $path"
   mkdir -p "$data_path/conf/live/$domain"
   docker-compose -f $composefile run --rm --entrypoint "\
     openssl req -x509 -nodes -newkey rsa:1024 -days 1\
@@ -53,6 +55,7 @@ do
     rm -Rf /etc/letsencrypt/renewal/$domain.conf" certbot
   echo
 done
+
 
 
 # Select appropriate email arg
